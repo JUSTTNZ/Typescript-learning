371647d0551ce8b7e79b006698d985b72f8d1ac3
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-03-09 05:46:25 +0100

    reinstalled npm npm WARN npm npm does not support Node.js v10.15.3

--- code/03 React TypeScript Essentials/01 Starting Project/src/App.tsx
+++ code/03 React TypeScript Essentials/01 Starting Project/src/App.tsx
@@ -1,8 +1,11 @@
+
 import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal 
+      title="Learn React + TS"
+      description="Learn it from the ground up"/>
     </main>
   )
 }

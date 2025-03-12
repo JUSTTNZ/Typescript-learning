2242c892aa5ec4c2144734148b7191ce59cbf05e
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-03-12 03:29:46 +0100

    handling user input in a typesafe way

--- code/03 React TypeScript Essentials/01 Starting Project/src/components/NewGoal.tsx
+++ code/03 React TypeScript Essentials/01 Starting Project/src/components/NewGoal.tsx
@@ -1,19 +1,28 @@
-import { type FormEvent } from "react";
-export default function newGoal() {
+import { useRef, type FormEvent } from "react";
+type NewGoalProps = {
+    onAddGoal: (goal: string, summary: string) =>  void;
+}
+export default function newGoal({onAddGoal}: NewGoalProps) {
+    const goal =  useRef<HTMLInputElement>(null);
+    const summary = useRef<HTMLInputElement>(null);
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        new FormData(e.currentTarget);
+        const enteredGoal  = goal.current!.value;
+        const enteredSummary = summary.current!.value
+
+        e.currentTarget.reset();
+        onAddGoal(enteredGoal, enteredSummary)
     }
     return  (
         <form onSubmit={handleSubmit}>
             <p>
                 <label htmlFor="goal">Your Goal</label>
-                <input type="text" name="goal" id="goal" />
+                <input type="text" name="goal" id="goal" ref={goal} />
             </p>
             <p>
                 <label htmlFor="summary">Short Summary</label>
-                <input type="text" name="" id="summary" />
+                <input type="text" name="" id="summary" ref={summary}/>
             </p>
             <p>
                 <button>Add Goal</button>

3537715f14900e24c30f3f3b1face05435e22f3e
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-03-12 04:06:37 +0100

    building a more flexible component

--- code/03 React TypeScript Essentials/01 Starting Project/src/components/CourseGoalList.tsx
+++ code/03 React TypeScript Essentials/01 Starting Project/src/components/CourseGoalList.tsx
@@ -1,12 +1,31 @@
 import CourseGoal from './CourseGoal';
 import { type CourseGoal as CGoal } from  '../App'
+import InfoBox from './InfoBox';
+import { type ReactNode } from 'react';
 type CourseGoalListProps = {
   goals:  CGoal[];
   onDeleteGoal: (id: number) => void;
 }
 export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListProps) {
+    if(goals.length === 0) {
+        return (
+            <InfoBox mode='hint'>
+                You have no Course Goals yet. Start adding Some!
+            </InfoBox>
+        )
+    }
+
+    let warningBox: ReactNode;
+
+    if(goals.length >= 4) {
+        warningBox = <InfoBox mode='warning'>
+            You're collecting a lot of goals. Don't put too much on your plate!
+        </InfoBox>
+    }
     return (
-        <div>
+        <>
+        ...
+            <div>
             <ul>
                 {goals.map((goal) => (
                 <li key={goal.id}>
@@ -18,6 +37,6 @@ export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListPro
                 
             </ul>
         </div>
-        
+        </>
     )
 }

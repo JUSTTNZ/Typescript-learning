ba498ed00b88b4aa6e12e638145794d25771abac
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-03-17 06:33:11 +0100

    managing interval with refs and useeffect cleanup function

--- code/06 Side Effects/01 Starting Project/src/components/Timer.tsx
+++ code/06 Side Effects/01 Starting Project/src/components/Timer.tsx
@@ -1,17 +1,31 @@
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
-  const [remainingState, setRemainingState] = useState(duration * 1000);
-  setInterval(function() {
-    setRemainingState(prevTime => prevTime - 50);
-  }, 50)
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingState] = useState(duration * 1000);
+
+  if(remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+  useEffect(() => {
+    interval.current = setInterval(function() {
+      setRemainingState((prevTime) => prevTime - 50);
+    }, 50);
+    return () => {
+      if(interval.current) {
+        clearInterval(interval.current);
+      }
+    };
+  }, []);
+  
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p><progress max={duration * 1000} value={remainingState} /></p>
-      <p>{}</p>
+      <p><progress max={duration * 1000} value={remainingTime} /></p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
